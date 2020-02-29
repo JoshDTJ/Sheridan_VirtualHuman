0756a6d18db30a14f5e036df131daefef7cc12ab
Joshua Jewer <joshdtjewer@gmail.com>
2020-02-29 14:34:14 -0500

    Initial Commit

---
+++ Assets/_VirtualHuman2020/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+   // Update is called once per frame
+    void Update()
+    {
+        //Closes the Game build, temperary placement should be moved.
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+
+    }
+}
